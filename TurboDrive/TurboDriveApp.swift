1378713b5a79e14de9ad8fdcc1c29820bb480332
okekedev <okekedev@outlook.com>
2023-12-29 02:56:09 -0600

    Initial Commit

---
+++ TurboDrive/TurboDriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  TurboDriveApp.swift
+//  TurboDrive
+//
+//  Created by Christian  on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TurboDriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
